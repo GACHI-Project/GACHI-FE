3a94d653c391d6dea81e240b85a4e792a5fce1c7
hyeeon <hyeon@swu.ac.kr>
2026-06-06 12:21:11 +0900

    🐛 fix: 탭 진입 시 자녀 목록 전역 초기화, 오타 수정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,31 @@
+import { useEffect } from 'react';
 import type { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { Tabs } from 'expo-router';
 import { View, StyleSheet } from 'react-native';
 import TabBar from '../../src/components/common/TabBar';
 import colors from '../../src/constants/colors';
+import { fetchChildren } from '../../src/api/child';
+import { useChildrenStore } from '../../src/store/childrenStore';
 
 const renderTabBar = ({ state, descriptors, navigation, insets }: BottomTabBarProps) => (
   <TabBar state={state} descriptors={descriptors} navigation={navigation} insets={insets} />
 );
 
-const TabsLayout = () => (
-  <View style={styles.root}>
-    <Tabs tabBar={renderTabBar} screenOptions={{ headerShown: false }} />
-  </View>
-);
+const TabsLayout = () => {
+  const setChildren = useChildrenStore((s) => s.setChildren);
+
+  useEffect(() => {
+    fetchChildren()
+      .then(setChildren)
+      .catch(() => {});
+  }, [setChildren]);
+
+  return (
+    <View style={styles.root}>
+      <Tabs tabBar={renderTabBar} screenOptions={{ headerShown: false }} />
+    </View>
+  );
+};
 
 export default TabsLayout;
 
